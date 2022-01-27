f75a2bf01fc8bfa96e891c4d9a5686e1b26a5244
Igor Decess <i.decess@comcast.net>
2022-01-26 20:33:57 -0700

    Add do-you-wanna-build-a-snowman suite

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -1,7 +1,33 @@
 var Snowman = require('./Snowman');
 
 class Human {
+  constructor(name) {
+    this.name = name;
+    this.wantsToBuildASnowman = true;
+    this.materials = {
+      snowballs: 0,
+      coal: 0,
+      buttons: 0,
+      carrots: 0
+    };
 
+  }
+  gatherMaterials(gatheredMaterials, amount) {
+    this.materials[gatheredMaterials] = this.materials[gatheredMaterials] + amount;
+  }
+
+  buildASnowman() {
+    var newSnowman = new Snowman(this.materials);
+      newSnowman.canWearMagicHat();
+      return newSnowman;
+  }
+
+  placeMagicHat() {
+    if (this.materials.coal < 2 || this.materials.buttons < 5 || this.materials.carrots < 1 || this.materials.nowballs < 2) {
+      return 'I guess I didn\'t build it correctly.';
+    }
+    return 'Woah, this snowman is coming to life!';
+  }
 }
 
 module.exports = Human;
--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,5 +1,18 @@
 class Snowman {
+  constructor(snowmanDetails) {
+    this.carrots = snowmanDetails.carrots;
+    this.coal = snowmanDetails.coal;
+    this.buttons = snowmanDetails.buttons;
+    this.snowballs = snowmanDetails.snowballs;
+    this.magicHat = false;
+  }
 
+  canWearMagicHat() {
+    this.magicHat = true;
+    if (this.coal < 2 || this.buttons < 5 || this.carrots < 1 || this.snowballs < 2) {
+      this.magicHat = false;
+    }
+  }
 }
 
 module.exports = Snowman;

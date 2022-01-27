821afc29c4e0fa4c460a620cfbb9e175295d6b6e
Igor Decess <i.decess@comcast.net>
2022-01-26 20:35:55 -0700

    Add lunchbox suite

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,26 @@
 class LunchBox {
+  constructor(lunchBoxObject) {
+    this.owner = lunchBoxObject.owner;
+    this.material = lunchBoxObject.madeOf;
+    this.shape = lunchBoxObject.shape;
+    this.color = lunchBoxObject.color;
+    this.snacks = [];
+  }
 
+  acquireSnack(someSnack) {
+    this.snacks.push(someSnack);
+    someSnack.isInLunchBox = true;
+  }
+
+  findHealthySnacks() {
+    var healthySnacks = [];
+    for (var i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i].checkForHealthy()) {
+        healthySnacks.push(this.snacks[i].type);
+      }
+    }
+    return healthySnacks;
+  }
 }
 
 module.exports = LunchBox;
--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,5 +1,26 @@
 class Snack {
+  constructor(type) {
+    this.deliciousLevel = "extra";
+    this.type = type;
+    this.amount = 100;
+    this.isInLunchBox = false;
+    this.cuttingItClose = false;
+  }
+  
+  getEaten() {
+    this.amount -= 10;
+    if (this.amount <= 20) {
+      this.cuttingItClose = true;
+    }
+  }
 
+  checkForHealthy() {
+    if (this.type.toLowerCase().includes('fruit')) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 module.exports = Snack;

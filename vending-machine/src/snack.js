aff883bad2b9015cba40ac2f7c64ebde44c5e0d2
Igor Decess <i.decess@comcast.net>
2022-01-26 20:57:41 -0700

    Add partial vending-machine suite

--- vending-machine/src/snack.js
+++ vending-machine/src/snack.js
@@ -1,5 +1,21 @@
 class Snack {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+    this.itemsInStock = 0;
+  }
 
+  stockItems(number) {
+    this.itemsInStock += number;
+  }
+
+  removeItem() {
+    if (this.itemsInStock > 0) {
+      this.itemsInStock--;
+      return `Item taken! There are now ${this.itemsInStock} items left.`
+    }
+    return `Sorry, no ${this.name} left in stock!`
+  }
 }
 
 module.exports = Snack;
--- vending-machine/src/vending-machine.js
+++ vending-machine/src/vending-machine.js
@@ -1,4 +1,29 @@
 class VendingMachine {
+  constructor(vendMichineObj) {
+    this.id = vendMichineObj.id;
+    this.isBroken = vendMichineObj.isBroken;
+    this.snacks = [];
+  }
+
+  addSnacks(snack) {
+    for (var i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i].name === snack.name) {
+        return 'Sorry, that snack is already stocked! Try adding a different snack.';
+      }
+    }
+    this.snacks.push(snack);
+  }
+
+  purchaseSnack(snack, money) {
+    for (var i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i].itemsInStock === snack.itemsInStock) {
+        this.snacks.splice(i, 1);
+        var change = money - snack.price;
+        return change;
+      }
+    }
+  }
+
 
 }
 

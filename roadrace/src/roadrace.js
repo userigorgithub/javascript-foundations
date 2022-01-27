e9f18776a90f12bdc3894e98a1929a130c239255
Igor Decess <i.decess@comcast.net>
2022-01-26 20:40:45 -0700

    Add roadrace suite

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,26 @@
 class Roadrace {
+  constructor(roadRaceObj) {
+    this.name = roadRaceObj.title;
+    this.location = roadRaceObj.city;
+    this.distance = 0;
+    this.participants = [];
+  }
 
+  setDistance(miles) {
+    this.distance += miles;
+    return `The ${this.name} in ${this.location} is a ${this.distance} mile race.`
+  }
+
+  registerParticipants(participant) {
+    this.participants.push(participant);
+  }
+
+  completeRace() {
+    for (var i = 0; i < this.participants.length; i++) {
+      this.participants[i].completedRaces = this.name;
+      this.participants[i].milesRun = this.distance;
+    }
+  }
 }
 
 module.exports = Roadrace;
--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,5 +1,28 @@
 class Runner {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.milesRun = 0;
+    this.fitness = 25;
+    this.completedRaces = [];
+  }
 
+  runSomeMiles(miles) {
+    this.milesRun += miles;
+    this.fitness = this.fitness + this.milesRun;
+  }
+
+  stretch() {
+    this.fitness += 0.5;
+  }
+
+  runRace(name, num) {
+    this.completedRaces.push(name);
+    // this.milesRun += num;
+    // this.fitness += num;
+    // OR more correct:
+    this.runSomeMiles(num);
+  }
 }
 
 module.exports = Runner;

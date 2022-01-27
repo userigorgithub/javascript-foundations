a8ae3f54b1d1e5af957c2aeb7f642deed8601be4
Igor Decess <i.decess@comcast.net>
2022-01-26 20:32:16 -0700

    Add crafting suite

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,20 @@
 class Craft {
+  constructor(craftObj) {
+    this.name = craftObj.type;
+    this.materials = craftObj.materials;
+    this.completed = false;
+  }
+  
+  completeCraft() {
+    this.completed = true;
+    return 'All done! It looks great!';
+  }
 
+  calculateProjectTotal() {
+    return this.materials[0].price * this.materials[0].amount + this.materials[1].price * this.materials[1].amount;
+
+//???? dynamic?
+  }
 }
 
 module.exports = Craft;
--- crafting/src/material.js
+++ crafting/src/material.js
@@ -1,5 +1,26 @@
 class Material {
+  constructor(name, price, amount, units) {
+    this.name = name;
+    this.price = price;
+    this.amount = amount;
+    this.units = units;
+  }
 
+  useMaterial(amount) {
+      if (this.amount >= amount) {
+        this.amount -= amount;
+        return `You now have ${this.amount} ${this.units} of ${this.name} left.`;
+      }
+      amount -= this.amount;
+      return `You don\'t have enough ${this.name}! Try using ${this.amount} ${this.units} or less.`
+  }
+
+  calculateMaterialCost() {
+    return this.price * this.amount;
+
+    // var cost = this.price * this.amount;
+    // return cost;
+  }
 }
 
 module.exports = Material;
